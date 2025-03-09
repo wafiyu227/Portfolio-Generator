a892b13a1a2283438bf94b8cec33f0dc76808665
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-09 09:53:05 -1200

    templates feature

---
+++ index.js
@@ -0,0 +1,5 @@
+function selectTemplate(templateNumber) {
+let selectedTemplate = document.getElementById(`template-${templateNumber}`);
+let clone = selectedTemplate.content.cloneNode(true);
+document.getElementById("container").appendChild(clone)
+}
