7cb04e13187158e55dbf69f42ec209cc0f2e9701
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-12 21:53:33 -1200

    footer

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function selectTemplate(templateNumber) {
-let selectedTemplate = document.getElementById(`template-${templateNumber}`);
-let clone = selectedTemplate.content.cloneNode(true);
-document.getElementById("container").appendChild(clone)
+  let selectedTemplate = document.getElementById(`template-${templateNumber}`);
+  let clone = selectedTemplate.content.cloneNode(true);
+  document.getElementById("container").appendChild(clone);
 }
-
