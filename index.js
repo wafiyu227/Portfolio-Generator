3664590ee629b41126b4a787b6b3b5c2326e548a
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-17 12:52:59 -1200

    customize

--- index.js
+++ index.js
@@ -11,18 +11,28 @@ function setupMainPage() {
   (templateContainer.style.boxShadow = "0px 4px 10px rgba(0, 0, 0, 0.5)"),
     (templateContainer.style.padding = "20px"),
     (templateContainer.style.borderRadius = "10px");
-  templateContainer.addEventListener("click", function () {
-    {
-      for (let i = 1; i <= 1; i++) {
-        let selectedTemplate = document.getElementById(`template-${i}`);
+
+  for (let i = 1; i <= 6; i++) {
+    document.getElementById(`card-${i}`).addEventListener("click", function () {
+      let template = document
+        .getElementById(`template-${i}`)
+        .content.cloneNode(true);
+
+      document.getElementById("container").appendChild(template);
+
+      const selectBtn = document.querySelector(".customize-btn");
+
+      selectBtn.addEventListener("click", function () {
+        const template = document.getElementById(`template-${i}`)
+        selectedTemplate = template;
 
         if (selectedTemplate) {
           localStorage.setItem("selectedTemplate", selectedTemplate.outerHTML);
           window.open("customize.html", "_blank");
         }
-      }
-    }
-  });
+      });
+    });
+  }
 }
 
 function loadCustomizePage() {
@@ -34,6 +44,7 @@ function loadCustomizePage() {
   } else {
     container.innerHTML = "<p>No template selected.</p>";
   }
+
   const selectBtn = document.querySelector(".customize-btn");
   selectBtn.style.display = "none";
 }
