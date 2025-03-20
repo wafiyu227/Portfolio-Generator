20dfc660677dd9ebcc03f7907e2016e5e368658f
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-20 10:28:44 -1200

    wip

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function loadTemplate(templateName) {
   const iframe = document.createElement("iframe");
   iframe.src = `templates/${templateName}.html/${templateName}`; // Load full template
-  iframe.style.width = "70%";
+  iframe.style.width = "100%";
   iframe.style.height = "100vh"; // Adjust height as needed
   iframe.style.border = "1px solid";
 
