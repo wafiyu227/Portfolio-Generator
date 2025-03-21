2de173cf098cf1c6cc84881048721dd33468f098
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-21 10:43:43 -1200

    sidebar

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function loadTemplate(templateName) {
   const iframe = document.createElement("iframe");
   iframe.src = `templates/${templateName}.html/${templateName}`; // Load full template
-  iframe.style.width = "100%";
+  iframe.style.width = "80%";
   iframe.style.height = "100vh"; // Adjust height as needed
   iframe.style.border = "1px solid";
+  iframe.style.display = "inline-block"
+  iframe.style.overflow = "hidden"
+
+  const sideBar = document.querySelector(".parent");
+  sideBar.style.position = "absolute"
+  sideBar.style.top = "40%";
+  sideBar.style.right = "60px"
+  
 
   const container = document.getElementById("portfolio-preview");
   container.innerHTML = ""; // Clear previous template
