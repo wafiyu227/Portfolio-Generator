ea7309dfb5556f0fd7b01ec75e431835058c4b0c
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-22 22:00:45 -1200

    wip

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ function loadTemplate(templateName) {
   iframe.style.width = "80%";
   iframe.style.height = "100vh"; // Adjust height as needed
   iframe.style.border = "1px solid";
-  document.querySelector(".customization").style.display = "inline-block"
-  
-
-  
+  document.querySelector(".customization").style.display = "inline-block";
 
   const container = document.getElementById("portfolio-preview");
   container.innerHTML = ""; // Clear previous template
   container.appendChild(iframe);
 }
+iframe.onload = function () {
+  applyColor(iframe);
+};
+
+
+function applyColor(iframe) {
+const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+if (iframeDoc) {
+  iframeDoc.body.style.backgroundColor = themes.value; // Apply color
+}
+}
+
+// color change event listener
+const themes = document.getElementById("fonts");
+fonts.addEventListener("change", function () {
+const iframe = document.querySelector("#portfolio-preview iframe");
+if (iframe) {
+  applyColor(iframe);
+}
+});
