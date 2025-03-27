6ada402138f9e783ce0b8c2f8fa87f682e5dba41
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-03-26 20:13:38 -1200

    links

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("home").addEventListener("click", () => {
+    window.location.href = "main.html";
+  });
+  document.getElementById("templates").addEventListener("click", () => {
+    window.location.href = "templates.html";
+  });
+  document.getElementById("contacts").addEventListener("click", () => {
+    window.location.href = "contacts.html";
+  });
+  document.getElementById("pricing").addEventListener("click", () => {
+    window.location.href = "pricing.html";
+  });
+});
+
 function loadTemplate(templateName) {
   const iframe = document.createElement("iframe");
-  iframe.src = `templates/${templateName}.html/${templateName}`; // Load full template
+  iframe.src = `templates/${templateName}.html/${templateName}`;
   iframe.style.width = "80%";
-  iframe.style.height = "100vh"; // Adjust height as needed
+  iframe.style.height = "100vh";
   iframe.style.border = "1px solid";
   document.querySelector(".customization").style.display = "inline-block";
 
   const container = document.getElementById("portfolio-preview");
-  container.innerHTML = ""; // Clear previous template
+  container.innerHTML = "";
   container.appendChild(iframe);
 }
 
